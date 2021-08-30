8b6c1dd783540b1732ba941e0577d828b45a318e
unknown <diaayuarofah35@gmail.com>
2021-08-30 07:18:01 +0700

    update time picker

--- src/containers/Widgets/timePicker.js
+++ src/containers/Widgets/timePicker.js
@@ -1,21 +1,11 @@
 // Add Time Picker
 
-import 'rc-time-picker/assets/index.css';
-
 import React from 'react';
-import ReactDom from 'react-dom';
-
 import moment from 'moment';
-
 import TimePicker from 'rc-time-picker';
-
 import confirm from './confirm.js';
 const format = 'h:mm a';
 const now = moment().hour(0).minute(0);
-
-function onChange(value) {
-  console.log(value && value.format(format));
-}
 
 const myDataObject = {
   arrival_time: "10:00"
