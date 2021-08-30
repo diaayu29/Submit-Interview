9fc2128839806a12a3a2376c7b3649301e3cf63b
unknown <diaayuarofah35@gmail.com>
2021-08-30 07:15:31 +0700

    update times

--- src/containers/Widgets/timePicker.js
+++ src/containers/Widgets/timePicker.js
@@ -1,4 +1,4 @@
-// Add time picker
+// Add Time Picker
 
 import 'rc-time-picker/assets/index.css';
 
@@ -13,8 +13,34 @@ import confirm from './confirm.js';
 const format = 'h:mm a';
 const now = moment().hour(0).minute(0);
 
+function onChange(value) {
+  console.log(value && value.format(format));
+}
+
+const myDataObject = {
+  arrival_time: "10:00"
+}
+
+// Update times
+
+const putData = async ( ) =>{
+  const response = await fetch('https://bv-online-assessment.herokuapp.com/api/bookings/:booking_code/update-eta', {
+      method: 'PUT', 
+      headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+      },
+      body: JSON.stringify(myDataObject)
+  });
+ 
+  const data = await response.json();
+  console.log(data);
+ };
+
+
 
 function Coba() {
+
   if(confirm === false) {
     return console.log("gagal")
   } else {
@@ -22,7 +48,8 @@ function Coba() {
     <TimePicker
       showSecond={false}
       defaultValue={now}
-      className="TimePicker"
+      className="xxx"
+      onChange={putData}
       format={format}
       use12Hours
       inputReadOnly
