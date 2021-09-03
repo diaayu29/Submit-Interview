450bfb1d225621a429576c3787c8e61d9ea3f9bd
unknown <diaayuarofah35@gmail.com>
2021-09-03 08:14:05 +0700

    tenth

--- src/helpers/firebase/index.js
+++ src/helpers/firebase/index.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 import ReduxSagaFirebase from 'redux-saga-firebase';
 import 'firebase/firestore';
 import { firebaseConfig } from '../../settings';
